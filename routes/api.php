92352722688a9f24b52e767ef3cf592efc240efa
Diego Garza <dgarza8988@gmail.com>
2017-03-29 19:01:59 -0600

    add: get list count

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Silverpop\EngagePod;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,27 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/list/{id}/count', function ($id) {
+    try {
+        $silverpop = new EngagePod([
+            'username' => env('SILVERPOP_API_USERNAME'),
+            'password' => env('SILVERPOP_API_PASSWORD'),
+            'engage_server' => env('SILVERPOP_API_POD')
+        ]);
+
+        $listMetaData = $silverpop->getListMetaData($id);
+
+        $response = [
+            'results' => [
+                'count' => $listMetaData['SIZE']
+            ]
+        ];
+
+        return $response;
+
+    } catch (Exception $e) {
+        throw $e;
+    };
 });
